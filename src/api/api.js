5b3e9a0b2cb6edfbe873a2bc8e18ed7071e9157a
ZaptoInc <38966434+ZaptoInc@users.noreply.github.com>
2024-01-28 12:39:57 +0100

    added Client.apiEndpoint to change the endpoint

--- src/api/api.js
+++ src/api/api.js
@@ -10,87 +10,90 @@ const Member = require("../structures/Member.js");
 const Application = require("../application/base");
 
 // Require the fastify application
-const Fastify = require('fastify');
+const Fastify = require("fastify");
 let app = Fastify({
-    logger: false,
-    trustProxy: true
+  logger: false,
+  trustProxy: true,
 });
 
-const express = require('express');
+const express = require("express");
 
 // Require the util class
 const Utils = require("../utils/Utils.js");
 const Util = new Utils();
 
 module.exports = async (Client) => {
-    Client.emit('debug', "[DEBUG] Starting Fastify Server");
+  Client.emit("debug", "[DEBUG] Starting Fastify Server");
 
-    let isCustomInstance = false;
-    if(Client.apiInstance !== null) {
-        isCustomInstance = true;
-        app = Client.apiInstance;
-    } else {
-        Client.apiInstance = app;
+  let isCustomInstance = false;
+  if (Client.apiInstance !== null) {
+    isCustomInstance = true;
+    app = Client.apiInstance;
+  } else {
+    Client.apiInstance = app;
+  }
+
+  /**
+   * Emitted the starting event.
+   * @event Application#starting
+   * @param {Application} c The Client
+   */
+  Client.emit("starting", Client);
+
+  app.post(`/${Client.apiEndpoint}`, async (req, res) => {
+    if (req.body === null || req.body === undefined) {
+      throw new Error(
+        "[Interactions.js => <Client>#interactionCreate] Your custom API Instance doesn't support the body-parser. Please use the default API Instance or install the body-parser package."
+      );
     }
 
+    Client.emit("debug", "[DEBUG] New Interaction " + req.body.id);
+
+    const verifyPayload = await Util.InteractionsMiddleware(Client, req);
+    if (!verifyPayload)
+      return Client.emit("debug", "[DEBUG] Didn't passed the verification. ");
+
+    if (req.body.type === InteractionType.PING) {
+      Client.emit("debug", "[DEBUG] Response with Pong " + req.body.id);
+      return res.send({ type: InteractionResponseType.PONG });
+    }
+
+    const interaction = new Interaction(req, Client, res);
+
+    Client.emit("debug", "[DEBUG] Forward Interaction " + req.body.id);
+
     /**
-     * Emitted the starting event.
-     * @event Application#starting
-     * @param {Application} c The Client
+     * Emitted the interaction event.
+     * @event Application#interactionCreate
+     * @param {Interaction} interaction The Interaction to handle
      */
-    Client.emit('starting', Client);
+    Client.emit("interactionCreate", interaction);
+  });
 
-    app.post('/interactions', async (req, res) => {
-        if(req.body === null || req.body === undefined) {
-            throw new Error("[Interactions.js => <Client>#interactionCreate] Your custom API Instance doesn't support the body-parser. Please use the default API Instance or install the body-parser package.");
-        }
+  if (!isCustomInstance) {
+    const host = Client.runOnAllInterfaces ? "0.0.0.0" : "localhost";
+    app.listen({ port: Client.port, host: host }, (err) => {
+      Client.emit("debug", "[DEBUG] API Online on port " + Client.port);
 
-        Client.emit('debug', "[DEBUG] New Interaction " + req.body.id);
+      Client.readySince = Date.now();
 
-        const verifyPayload = await Util.InteractionsMiddleware(Client, req);
-        if (!verifyPayload) return Client.emit('debug', "[DEBUG] Didn't passed the verification. ");
-
-        if (req.body.type === InteractionType.PING) {
-            Client.emit('debug', "[DEBUG] Response with Pong " + req.body.id);
-            return res.send({type: InteractionResponseType.PONG});
-        }
-
-        const interaction = new Interaction(req, Client, res);
-
-        Client.emit('debug', "[DEBUG] Forward Interaction " + req.body.id);
-
-        /**
-         * Emitted the interaction event.
-         * @event Application#interactionCreate
-         * @param {Interaction} interaction The Interaction to handle
-         */
-        Client.emit('interactionCreate', interaction);
-    });
-
-    if(!isCustomInstance) {
-        const host = Client.runOnAllInterfaces ? '0.0.0.0' : 'localhost';
-        app.listen({port: Client.port, host: host },  (err) => {
-            Client.emit('debug', "[DEBUG] API Online on port " + Client.port);
-
-            Client.readySince = Date.now();
-
-            setTimeout(() => {
-                /**
-                 * Emitted the ready event.
-                 * @event Application#ready
-                 * @param {Application} Client The Client
-                 */
-                Client.emit('ready', Client);
-            }, 0);
-        });
-    } else {
-        Client.readySince = Date.now();
-
+      setTimeout(() => {
         /**
          * Emitted the ready event.
          * @event Application#ready
          * @param {Application} Client The Client
          */
-        Client.emit('ready', Client);
-    }
+        Client.emit("ready", Client);
+      }, 0);
+    });
+  } else {
+    Client.readySince = Date.now();
+
+    /**
+     * Emitted the ready event.
+     * @event Application#ready
+     * @param {Application} Client The Client
+     */
+    Client.emit("ready", Client);
+  }
 };
--- src/application/base.js
+++ src/application/base.js
@@ -1,8 +1,8 @@
 const startAPI = require("../api/api.js");
-const mongooseConnectionHelper = require("../mongo/mongoose.js")
-const EventEmitter = require('node:events');
+const mongooseConnectionHelper = require("../mongo/mongoose.js");
+const EventEmitter = require("node:events");
 const Rest = require("../structures/Rest.js");
-const {Routes} = require('discord-api-types/v10');
+const { Routes } = require("discord-api-types/v10");
 const CacheManager = require("../structures/managers/CacheManager");
 const User = require("../structures/User");
 
@@ -37,214 +37,242 @@ const User = require("../structures/User");
  * @return {Application} The application
  */
 class Application extends EventEmitter {
-    constructor(options) {
-        super();
-        /**
-         * the token of the bot application (needed)
-         * @type {string}
-         */
-        this.botToken = options?.botToken ?? null;
-
-        /**
-         * the public key of the application (needed)
-         * @type {string}
-         */
-        this.publicKey = options?.publicKey ?? null;
-
-        /**
-         * the ID of the application (needed)
-         * @type {string}
-         */
-        this.applicationId = options?.applicationId ?? null;
-
-        /**
-         * the mongoose connection string (not needed)
-         * @type {string}
-         */
-        this.mongooseString = options?.mongooseString ?? null;
-
-        /**
-         * the port for the application (default is "1337")
-         * @type {number}
-         */
-        this.port = options?.port ?? 1337;
-
-        /**
-         * boolean to enable or disable the client channels cache
-         * @type {boolean}
-         */
-        this.cacheChannels = options?.cacheChannels ?? false;
-
-        /**
-         * boolean to enable or disable the client users cache
-         * @type {boolean}
-         */
-        this.cacheUsers = options?.cacheUsers ?? false;
-
-        /**
-         * boolean to enable or disable the client members cache
-         * @type {boolean}
-         */
-        this.cacheMembers = options?.cacheMembers ?? false;
-
-        /**
-         * boolean to enable or disable the client guilds cache
-         * @type {boolean}
-         */
-        this.cacheGuilds = options?.cacheGuilds ?? false;
-
-        /**
-         * boolean to enable or disable the client roles cache
-         * @type {boolean}
-         */
-        this.cacheRoles = options?.cacheRoles ?? false;
-
-        /**
-         * boolean to enable or disable that interactions.js use the mongoose connection to save and load cache
-         * @type {*|boolean}
-         */
-        this.useMongooseCache = options?.useMongooseCache ?? false;
-
-        /**
-         * set a custom cool down in milliseconds to save the cache in the mongoose database
-         * @type {*|number}
-         */
-        this.customCacheCooldown = options?.customCacheCooldown ?? 10 * 60 * 1000;
-
-        /**
-         * whether to run the application on all interfaces or not - needed for docker
-         * @type {boolean}
-         */
-        this.runOnAllInterfaces = options?.runOnAllInterfaces ?? false;
-        
-        /**
-         * if you want to use your own express or fastify instance
-         * @type {*|null}
-         */
-        this.apiInstance = options?.apiInstance ?? null;
-
-        /**
-         * Private property to store the client cache
-         * @type {CacheManager} the cache manager
-         * @private
-         */
-        this._cache = new CacheManager(this);
-
-        /**
-         * The unix timestamp since when the bot is ready
-         * @type {number|null}
-         */
-        this.readySince = null;
-
-        // Adding some ENV Data
-        process.env.DISCORD_TOKEN = this.botToken;
-        process.env.MONGOOSE_STRING = this.mongooseString;
-        process.env.PUBLIC_KEY = this.publicKey;
-        process.env.APPLICATION_ID = this.applicationId;
-
-        // Fetch the client data
-        if (options?.fetchClient) this.fetchClient();
-    }
-
-
-    fetchClient() {
-        const rest = Rest.getRest();
-
-        rest
-            .get(Routes.user(this.applicationId))
-            .then(data => {
-                this.emit('debug', "[DEBUG] Got the client data from the API");
-                this.user = new User(data);
-            }).catch(e => {
-                this.emit('debug', "[DEBUG] Got a error by fetching the client data from the API!" + e);
-                console.log(e)
-        });
-    }
+  constructor(options) {
+    super();
+    /**
+     * the token of the bot application (needed)
+     * @type {string}
+     */
+    this.botToken = options?.botToken ?? null;
 
     /**
-     * Start the application
-     *
-     * @param token the bot token if not already set as "botToken" in the application creation
+     * the public key of the application (needed)
+     * @type {string}
      */
-    async start(token = this.botToken) {
-        if (!this.publicKey || !this.applicationId) {
-            throw new Error("[Interactions.js => <Client>.start] Make sure to specify a valid publicKey and applicationId!");
-        }
-
-        this.emit('debug', "[DEBUG] Loading App");
-
-        this.botToken = token;
-
-        if (!this.mongooseString && typeof this.mongooseString === "string") await mongooseConnectionHelper.init(this);
-
-        await startAPI(this);
-    }
-
+    this.publicKey = options?.publicKey ?? null;
 
     /**
-     * Set the Slash Commands for the Application
-     *
-     * @param {array} arrayOfSlashCommands an array of slash commands to set
+     * the ID of the application (needed)
+     * @type {string}
      */
-    async setAppCommands(arrayOfSlashCommands) {
-        if (!this.botToken) throw new Error("[Interactions.js => <Client>.setAppCommands] You need to provide a valid token.");
-
-        if (!this.applicationId) throw new Error("[Interactions.js => <Client>.setAppCommands] You need to provide a valid applicationId.");
-
-        const rest = Rest.getRest();
-
-        try {
-            await rest.put(
-                Routes.applicationCommands(this.applicationId),
-                {body: arrayOfSlashCommands},
-            );
-
-            this.emit('debug', "[DEBUG] Posted Slash Commands");
-
-            return true;
-        } catch (error) {
-            this.emit('debug', "[DEBUG] Got a error by posting Slash Commands!");
-
-            return {
-                error: true,
-                errorData: error
-            }
-        }
-    }
+    this.applicationId = options?.applicationId ?? null;
 
     /**
-     * Set the Slash Commands for a Guild
-     *
-     * @param {array} arrayOfSlashCommands an array of slash commands to set
-     * @param {string} GuildId the guild id to post the commands to
+     * the mongoose connection string (not needed)
+     * @type {string}
      */
-    async setGuildCommands(arrayOfSlashCommands, GuildId) {
-        if (!this.botToken) throw new Error("[Interactions.js => <Client>.setGuildCommands] You need to provide a valid token.");
+    this.mongooseString = options?.mongooseString ?? null;
 
-        if (!this.applicationId) throw new Error("[Interactions.js => <Client>.setGuildCommands] You need to provide a valid applicationId.");
+    /**
+     * the port for the application (default is "1337")
+     * @type {number}
+     */
+    this.port = options?.port ?? 1337;
 
-        if (!GuildId) throw new Error("[Interactions.js => <Client>.setGuildCommands] You need to provide a valid GuildId.");
+    /**
+     * boolean to enable or disable the client channels cache
+     * @type {boolean}
+     */
+    this.cacheChannels = options?.cacheChannels ?? false;
 
-        const rest = Rest.getRest();
+    /**
+     * boolean to enable or disable the client users cache
+     * @type {boolean}
+     */
+    this.cacheUsers = options?.cacheUsers ?? false;
 
-        try {
-            await rest.put(
-                Routes.applicationGuildCommands(this.applicationId, GuildId),
-                {body: arrayOfSlashCommands},
-            );
+    /**
+     * boolean to enable or disable the client members cache
+     * @type {boolean}
+     */
+    this.cacheMembers = options?.cacheMembers ?? false;
 
-            this.emit('debug', "[DEBUG] Posted Slash Commands to " + GuildId);
+    /**
+     * boolean to enable or disable the client guilds cache
+     * @type {boolean}
+     */
+    this.cacheGuilds = options?.cacheGuilds ?? false;
 
-            return true;
-        } catch (error) {
-            this.emit('debug', "[DEBUG] Got a error by posting Slash Commands to " + GuildId + "!");
+    /**
+     * boolean to enable or disable the client roles cache
+     * @type {boolean}
+     */
+    this.cacheRoles = options?.cacheRoles ?? false;
 
-            return {
-                error: true,
-                errorData: error
-            }
-        }
+    /**
+     * boolean to enable or disable that interactions.js use the mongoose connection to save and load cache
+     * @type {*|boolean}
+     */
+    this.useMongooseCache = options?.useMongooseCache ?? false;
+
+    /**
+     * set a custom cool down in milliseconds to save the cache in the mongoose database
+     * @type {*|number}
+     */
+    this.customCacheCooldown = options?.customCacheCooldown ?? 10 * 60 * 1000;
+
+    /**
+     * whether to run the application on all interfaces or not - needed for docker
+     * @type {boolean}
+     */
+    this.runOnAllInterfaces = options?.runOnAllInterfaces ?? false;
+
+    /**
+     * if you want to use your own express or fastify instance
+     * @type {*|null}
+     */
+    this.apiInstance = options?.apiInstance ?? null;
+
+    /**
+     * if you want to use another api endpoint
+     * @type {string}
+     */
+    this.apiEndpoint = options?.apiInstance ?? "";
+
+    /**
+     * Private property to store the client cache
+     * @type {CacheManager} the cache manager
+     * @private
+     */
+    this._cache = new CacheManager(this);
+
+    /**
+     * The unix timestamp since when the bot is ready
+     * @type {number|null}
+     */
+    this.readySince = null;
+
+    // Adding some ENV Data
+    process.env.DISCORD_TOKEN = this.botToken;
+    process.env.MONGOOSE_STRING = this.mongooseString;
+    process.env.PUBLIC_KEY = this.publicKey;
+    process.env.APPLICATION_ID = this.applicationId;
+
+    // Fetch the client data
+    if (options?.fetchClient) this.fetchClient();
+  }
+
+  fetchClient() {
+    const rest = Rest.getRest();
+
+    rest
+      .get(Routes.user(this.applicationId))
+      .then((data) => {
+        this.emit("debug", "[DEBUG] Got the client data from the API");
+        this.user = new User(data);
+      })
+      .catch((e) => {
+        this.emit(
+          "debug",
+          "[DEBUG] Got a error by fetching the client data from the API!" + e
+        );
+        console.log(e);
+      });
+  }
+
+  /**
+   * Start the application
+   *
+   * @param token the bot token if not already set as "botToken" in the application creation
+   */
+  async start(token = this.botToken) {
+    if (!this.publicKey || !this.applicationId) {
+      throw new Error(
+        "[Interactions.js => <Client>.start] Make sure to specify a valid publicKey and applicationId!"
+      );
     }
+
+    this.emit("debug", "[DEBUG] Loading App");
+
+    this.botToken = token;
+
+    if (!this.mongooseString && typeof this.mongooseString === "string")
+      await mongooseConnectionHelper.init(this);
+
+    await startAPI(this);
+  }
+
+  /**
+   * Set the Slash Commands for the Application
+   *
+   * @param {array} arrayOfSlashCommands an array of slash commands to set
+   */
+  async setAppCommands(arrayOfSlashCommands) {
+    if (!this.botToken)
+      throw new Error(
+        "[Interactions.js => <Client>.setAppCommands] You need to provide a valid token."
+      );
+
+    if (!this.applicationId)
+      throw new Error(
+        "[Interactions.js => <Client>.setAppCommands] You need to provide a valid applicationId."
+      );
+
+    const rest = Rest.getRest();
+
+    try {
+      await rest.put(Routes.applicationCommands(this.applicationId), {
+        body: arrayOfSlashCommands,
+      });
+
+      this.emit("debug", "[DEBUG] Posted Slash Commands");
+
+      return true;
+    } catch (error) {
+      this.emit("debug", "[DEBUG] Got a error by posting Slash Commands!");
+
+      return {
+        error: true,
+        errorData: error,
+      };
+    }
+  }
+
+  /**
+   * Set the Slash Commands for a Guild
+   *
+   * @param {array} arrayOfSlashCommands an array of slash commands to set
+   * @param {string} GuildId the guild id to post the commands to
+   */
+  async setGuildCommands(arrayOfSlashCommands, GuildId) {
+    if (!this.botToken)
+      throw new Error(
+        "[Interactions.js => <Client>.setGuildCommands] You need to provide a valid token."
+      );
+
+    if (!this.applicationId)
+      throw new Error(
+        "[Interactions.js => <Client>.setGuildCommands] You need to provide a valid applicationId."
+      );
+
+    if (!GuildId)
+      throw new Error(
+        "[Interactions.js => <Client>.setGuildCommands] You need to provide a valid GuildId."
+      );
+
+    const rest = Rest.getRest();
+
+    try {
+      await rest.put(
+        Routes.applicationGuildCommands(this.applicationId, GuildId),
+        { body: arrayOfSlashCommands }
+      );
+
+      this.emit("debug", "[DEBUG] Posted Slash Commands to " + GuildId);
+
+      return true;
+    } catch (error) {
+      this.emit(
+        "debug",
+        "[DEBUG] Got a error by posting Slash Commands to " + GuildId + "!"
+      );
+
+      return {
+        error: true,
+        errorData: error,
+      };
+    }
+  }
 }
 
 module.exports = Application;
